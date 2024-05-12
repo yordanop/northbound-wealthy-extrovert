ea089cb14185b6677493593ddd25ad9f265e4798
Eynar <yordanopv@gmail.com>
2024-05-12 01:45:53 -0600

    adding buttons to hist

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -44,44 +44,79 @@ function getGeoInfo(cityName){
     });
 }
 
+function createHistButton(cityName){
+    const buttonsContainer = document.querySelector('#history-container')
+    const blockDiv = document.createElement('div');
+    const menuList = document.createElement('li');
+    const newButton = document.createElement('button');
+
+    blockDiv.setAttribute('class', 'block');
+    newButton.setAttribute('class', 'button has-background-info-soft');
+    newButton.innerHTML = cityName;
+
+    menuList.appendChild(newButton);
+    blockDiv.appendChild(menuList);
+
+    buttonsContainer.appendChild(blockDiv);
+}
 
 window.addEventListener('DOMContentLoaded', function(event){
-    const hum
+    const humidMain = document.querySelector('#hum-main');
+    const tempMain = document.querySelector('#temp-main');
+    const windMain = document.querySelector('#wind-main');
+    const dateMain = this.document.querySelector('#main-date');
+
     // searchBar.addEventListener('keypress', function(enterKey){
     //     cityNameInput = searchBar.value;
         
-    //     if(enterKey.key === 'Enter'){
-            // getGeoInfo(cityNameInput)
-            //     .then(function(coordinates) {
-                //         if(coordinates !== undefined){
-                    //             getWeatherInfo(coordinates)
-            //                 .then(function(data_1){
+        // if(enterKey.key === 'Enter'){
+        //     getGeoInfo(cityNameInput)
+        //         .then(function(coordinates) {
+        //                 if(coordinates !== undefined){
+        //                         getWeatherInfo(coordinates)
+        //                     .then(function(data_1){
 
-            //                     let nameData = data_1.city.name;
+        //                         let nameData = data_1.city.name;
             
-            //                     countriesInfo.push({[nameData]:data_1.list});
+        //                         countriesInfo.push({
+        //                             countryName : nameData,
+        //                             info:data_1.list
+        //                         });
             
-            //                     localStorage.setItem('allCountries', JSON.stringify(countriesInfo));
-            //                     cityTitle.textContent = `${nameData} Info`;
-            //                     cityInfoTitle.textContent = nameData;                                                    
-            //                 }
-            //             );
-            //         }else{
-            //             searchBar.value = '';
-            //         }
-            //     }
-            // );
-            
-            
-            
+        //                         localStorage.setItem('allCountries', JSON.stringify(countriesInfo));
+        //                         cityTitle.textContent = `${nameData} Info`;
+        //                         cityInfoTitle.textContent = nameData;                                                    
+        //                     }
+        //                 );
+        //             }else{
+        //                 searchBar.value = '';
+        //             }
+        //         }
+        //     );
             
         //     searchBar.value = '';
         // }
-        let data_1 = JSON.parse(localStorage.getItem('allCountries'))[0];
-        let nameData = 'Mexico';
+
+
         
-        cityTitle.textContent = `${nameData} Info`;
-        cityInfoTitle.textContent = nameData;
     // })
+    let data_1 = JSON.parse(localStorage.getItem('allCountries'))[0];
+    let nameData = data_1.countryName;
+    let basicInfo = data_1.info[0];
+    const todayVar = dayjs(basicInfo.dt_text).format('MM/DD/YYYY');
+    
+    dateMain.innerHTML = `(${todayVar})`;
+    cityTitle.textContent = `${nameData} Info`;
+    cityInfoTitle.textContent = nameData;
+
+    humidMain.textContent = `Humidity : ${basicInfo.main.humidity}%`;
+    tempMain.textContent = `Temperature : ${basicInfo.main.temp} °F`;
+    windMain.textContent = `Wind : ${basicInfo.wind.speed} MPH`;
+
+
+    for (let day_i = 8; day_i < data_1.info.length; day_i += 8){
+        basicInfo = data_1.info[day_i];
+        createForecastCard(basicInfo);
+    }
 
 })
